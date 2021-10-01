be22886c0ced409afaee8ea4fd2b56874680b281
Tijani <tijanitrabajo@gmail.com>
2021-10-01 20:10:13 +0200

    Start Project

--- app/src/main/java/com/tijanidian/pmpd_playground/MainActivity.kt
+++ app/src/main/java/com/tijanidian/pmpd_playground/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tijanidian.pmpd_playground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.content.ContextCompat
 
@@ -19,5 +20,32 @@ class MainActivity : AppCompatActivity() {
         imageLion.setImageResource(R.drawable.ic_leon)
 
 
+
     }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("@dev","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("@dev","onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d("@dev","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("@dev","onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("@dev","onDestroy")
+    }
+
 }
