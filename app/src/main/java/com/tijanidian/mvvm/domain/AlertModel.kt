736d180ee9846092f828182eee392eae3f84817e
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-11-05 19:30:41 +0100

    Ejecutamos los casos de uso

--- app/src/main/java/com/tijanidian/mvvm/domain/AlertModel.kt
+++ app/src/main/java/com/tijanidian/mvvm/domain/AlertModel.kt
@@ -2,10 +2,13 @@ package com.tijanidian.mvvm.domain
 
 
 data class AlertModel (
-    val id: String, val title: String,
-    val type: Int, val summary: String,
+    val id: String,
+    val title: String,
+    val type: Int,
+    val summary: String,
     val datePublished: String,
-    val body: String, val source: String,
+    val body: String,
+    val source: String,
     val files: List<FileEntity>
     )
 
