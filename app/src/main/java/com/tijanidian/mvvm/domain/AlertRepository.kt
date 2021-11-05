3460fc303cef3081eb3caeded0c836b7a5f3fc96
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-11-05 19:31:28 +0100

    Funcion para obtener un listado de AlertModel

--- app/src/main/java/com/tijanidian/mvvm/domain/AlertRepository.kt
+++ app/src/main/java/com/tijanidian/mvvm/domain/AlertRepository.kt
@@ -1,8 +1,5 @@
 package com.tijanidian.mvvm.domain
 
 interface AlertRepository {
-    fun getMvvm():AlertModel
-    fun saveMvvm(alertModel: AlertModel)
-    fun removeMvvm(id:String)
-    fun getMvvmAll():List<AlertModel>
+    fun getAlerts() : List<AlertModel>
 }
