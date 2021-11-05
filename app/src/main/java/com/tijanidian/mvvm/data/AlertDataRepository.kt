5de8ee23e35b1a313d6f67b610a3363313f7e6df
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-11-05 19:35:03 +0100

    datos almacenados

--- app/src/main/java/com/tijanidian/mvvm/data/AlertDataRepository.kt
+++ app/src/main/java/com/tijanidian/mvvm/data/AlertDataRepository.kt
@@ -2,21 +2,17 @@ package com.tijanidian.mvvm.data
 
 import com.tijanidian.mvvm.domain.AlertModel
 import com.tijanidian.mvvm.domain.AlertRepository
+import com.tijanidian.mvvm.domain.FileEntity
 
-class AlertDataRepository: AlertRepository {
-    override fun getMvvm(): AlertModel {
-        TODO("Not yet implemented")
-    }
-
-    override fun saveMvvm(alertModel: AlertModel) {
-        TODO("Not yet implemented")
-    }
-
-    override fun removeMvvm(id: String) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getMvvmAll(): List<AlertModel> {
-        TODO("Not yet implemented")
-    }
+class AlertDataRepository : AlertRepository {
+    override fun getAlerts(): List<AlertModel> = mutableListOf(AlertModel(
+        "1",
+        "title01",
+        1,
+        "prueba",
+        "04-11-21",
+        "prueba",
+        "recurso",
+        mutableListOf()
+    ))
 }
