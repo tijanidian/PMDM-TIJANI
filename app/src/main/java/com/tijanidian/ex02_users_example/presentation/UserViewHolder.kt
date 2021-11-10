5fe2730d2cf61a9de3d4c0dbb9503198cecff578
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-11-10 18:53:16 +0100

    Ejemplo de mvvm users con la implementación de RecyclerView

---
+++ app/src/main/java/com/tijanidian/ex02_users_example/presentation/UserViewHolder.kt
@@ -0,0 +1,18 @@
+package com.tijanidian.ex02_users_example.presentation
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.tijanidian.pmpd_playground.databinding.ViewItemUt02Ex02Binding
+
+//Recibe por constructor una vista y extiende de Reclycler
+class UserViewHolder(view:View):RecyclerView.ViewHolder(view) {
+    private val bind=ViewItemUt02Ex02Binding.bind(view)
+
+    fun render(userViewState: UserViewState){
+            bind.labelName.text=userViewState.fullName
+            bind.labelAge.text=userViewState.age
+
+    }
+
+
+}
