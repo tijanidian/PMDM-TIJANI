2849c6b3c51b8c7a7592ea3fd15dcc5b67a16e80
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-11-21 14:15:27 +0100

    Creado caso de uso para conseguir un id especifico

--- app/src/main/java/com/tijanidian/ex02_alerts_recyclerview/domain/FindAlertUseCase.kt
+++ app/src/main/java/com/tijanidian/ex02_alerts_recyclerview/domain/FindAlertUseCase.kt
@@ -2,7 +2,7 @@ package com.tijanidian.ex02_alerts_recyclerview.domain
 
 class FindAlertUseCase(private val repository:AlertRepository) {
 
-    fun execute(id:String):List<AlertModel>{
+    fun execute(id:String):AlertModel?{
         return repository.getAlert(id)
     }
 
