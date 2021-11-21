e037ed3b889d341ecc268d9d79741e61bfa286a2
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-11-21 14:45:32 +0100

    Implemented ApiClient.kt

--- app/src/main/java/com/tijanidian/ex02_alerts_recyclerview/app/ApiClient.kt
+++ app/src/main/java/com/tijanidian/ex02_alerts_recyclerview/app/ApiClient.kt
@@ -1,9 +1,9 @@
 package com.tijanidian.ex02_alerts_recyclerview.app
 
 import com.tijanidian.ex02_alerts_recyclerview.data.AlertApiModel
+import com.tijanidian.ex02_alerts_recyclerview.data.DetailAlertApiModel
 
 interface ApiClient {
     fun getAlerts():List<AlertApiModel>
-    fun getAlert(id:String):AlertApiModel?
-
+    fun getAlert(id:String):DetailAlertApiModel?
 }
