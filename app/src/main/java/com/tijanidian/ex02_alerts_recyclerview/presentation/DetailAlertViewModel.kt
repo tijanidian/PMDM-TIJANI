2b4ece9a49d8416d30df657c6f41345d0aa4cd46
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-11-21 14:42:33 +0100

    Implemented viewModel DetailAlertViewModel.kt

--- app/src/main/java/com/tijanidian/ex02_alerts_recyclerview/presentation/DetailAlertViewModel.kt
+++ app/src/main/java/com/tijanidian/ex02_alerts_recyclerview/presentation/DetailAlertViewModel.kt
@@ -1,22 +1,25 @@
 package com.tijanidian.ex02_alerts_recyclerview.presentation
 
 import androidx.lifecycle.ViewModel
+import com.tijanidian.ex02_alerts_recyclerview.domain.AlertModel
 import com.tijanidian.ex02_alerts_recyclerview.domain.FindAlertUseCase
 
 class DetailAlertViewModel(private val useCase: FindAlertUseCase) : ViewModel() {
 
-    fun getALert(id: String): List<AlertDatailViewState> {
+    fun getAlert(id: String): AlertDatailViewState? {
         val alert = useCase.execute(id)
-        return alert.map { alertModel ->
-            AlertDatailViewState(
-                alertModel.id,
-                alertModel.title,
-                alertModel.datePublished,
-                alertModel.summary,
-                alertModel.body
-
-            )
+        alert.let {
+            if (alert != null) {
+                return AlertDatailViewState(
+                    alert.id,
+                    alert.title,
+                    alert.type,
+                    alert.datePublished,
+                    alert.summary,
+                    alert.body
+                )
+            }
+            return null
         }
-
     }
 }
