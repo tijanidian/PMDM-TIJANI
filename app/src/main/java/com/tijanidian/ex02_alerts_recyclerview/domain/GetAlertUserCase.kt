2237f4d8cd76f2bc911d620a71baf4e04793c4ac
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-11-21 14:17:41 +0100

    Created use case to get alerts

--- app/src/main/java/com/tijanidian/ex02_alerts_recyclerview/domain/GetAlertUserCase.kt
+++ app/src/main/java/com/tijanidian/ex02_alerts_recyclerview/domain/GetAlertUserCase.kt
@@ -2,6 +2,6 @@ package com.tijanidian.ex02_alerts_recyclerview.domain
 
 class GetAlertUserCase(private val repository: AlertRepository) {
     fun execute():List<AlertModel>{
-        return repository.getALlAlerts()
+        return repository.getAlerts()
     }
 }
