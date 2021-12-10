a9a8d6d558770ba8f2bd8eb8251a4fe50c3089d3
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-12-10 12:22:51 +0100

    removed idPlayer

--- app/src/main/java/com/tijanidian/pmpd_playground/ut2/ut02ex06form/domain/Models.kt
+++ app/src/main/java/com/tijanidian/pmpd_playground/ut2/ut02ex06form/domain/Models.kt
@@ -1,7 +1,6 @@
 package com.tijanidian.pmpd_playground.ut2.ut02ex06form.domain
 
 data class PlayerModelFootball(
-    val idPlayer:Int,
     val name:String,
     val surname:String,
     val community:String,
